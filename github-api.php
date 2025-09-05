e9829955939020f23f8d2405ced3ca2d577d7aac
Rory Dale <rorydale@mac.com>
2025-09-05 14:46:23 -0400

    Removed PHP 8 only functions

--- github-api.php
+++ github-api.php
@@ -18,10 +18,14 @@ function loadDotEnv($path)
         if ($pos === false) continue;
         $key = trim(substr($line, 0, $pos));
         $val = trim(substr($line, $pos + 1));
-        // Strip surrounding quotes
-        if ((str_starts_with($val, '"') && str_ends_with($val, '"')) ||
-            (str_starts_with($val, "'") && str_ends_with($val, "'"))) {
-            $val = substr($val, 1, -1);
+        // Strip surrounding matching quotes (PHP 7 compatible)
+        $len = strlen($val);
+        if ($len >= 2) {
+            $first = $val[0];
+            $last  = $val[$len - 1];
+            if (($first === '"' && $last === '"') || ($first === "'" && $last === "'")) {
+                $val = substr($val, 1, $len - 2);
+            }
         }
         // Export to env and superglobals
         putenv("$key=$val");
